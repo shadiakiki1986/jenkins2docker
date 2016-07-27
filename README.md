04da77d8718a1f4e4ebb90b0ac3b55d44ceadcbb
shadiakiki1986 <shadiakiki1986@gmail.com>
2016-07-27 18:02:30 +0000

    add exporting config files

--- README.md
+++ README.md
@@ -2,16 +2,8 @@
 Jenkins server in FFA
 
 # Run
-1. `docker-compose up`
- * currently I have trouble with the file ownership in `/var/jenkins_home`
-2. then manually go to `configure jenkins` and install `ssh slaves plugin`
-3. configure the php7, add credentials (todo to be copied)
-4. launch slave agent
+`docker-compose up`
 
 # Back-up configs
-```bash
-docker-compose exec master cat /var/jenkins_home/nodes/php7/config.xml > jenkins_home/nodes/php7/config.xml
-docker-compose exec master cat /var/jenkins_home/jobs/ffa-php-core/config.xml > jenkins_home/jobs/ffa-php-core/config.xml
-docker-compose exec master cat /var/jenkins_home/users/shadi/config.xml > jenkins_home/users/shadi/config.xml
+`bash export.sh jenkins_home`
 
-```
---
+++ export.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [[ -z "$1" ]]; then
+  echo "Usage: export.sh jenkins_home"
+  echo "       export.sh ../shadi-configs/ffa24/jenkins_home/"
+  exit 1
+fi
+
+if [[ ! -d "$1" ]]; then
+  echo "Inexistant target folder: '$1'"
+  exit 1
+fi
+
+TARGET=$1
+#------------------------
+# Copy simple files
+function doit() {
+  mkdir -p $TARGET/$(dirname $1)
+  docker-compose exec master cat /var/jenkins_home/$1 > $TARGET/$1
+}
+
+doit nodes/php7/config.xml
+doit jobs/ffa-php-core/config.xml
+doit users/shadi/config.xml
+doit credentials.xml
+
+#------------------------
+# now export the secrets, as referenced here http://stackoverflow.com/a/31672340/4126114
+# but there is no docker-compose cp yet, as referenced here https://github.com/docker/compose/issues/3593
+function grepCont() {
+  docker ps|grep jenkins2dockershadi_master
+}
+NCONT=``
+if [ "$(grepCont|wc -l)" != "1" ]; then
+  echo "More than one jenkins master found. Aborting"
+  exit 1
+fi
+docker cp `grepCont|awk '{print $1}'`:/var/jenkins_home/secrets $TARGET
+
