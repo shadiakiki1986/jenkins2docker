3b722270dd5a57c5283fb46d04f800248a4683a0
shadiakiki1986 <shadiakiki1986@gmail.com>
2016-07-27 02:13:17 +0000

    add php7 and factor out common shell script

---
+++ slave_core.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+apk --update add openjdk7-jre openssh git && \
+    rm -rf /var/cache/apk/* 
+
+/usr/bin/java -version
+
+addgroup jenkins && \
+    adduser -D jenkins -s /bin/sh -G jenkins && \
+    chown -R jenkins:jenkins /home/jenkins && \
+    echo "jenkins:jenkins" | chpasswd
+
+ssh-keygen -A
+
+set -x && \
+    echo "UsePrivilegeSeparation no" >> /etc/ssh/sshd_config && \
+    echo "PermitRootLogin no" >> /etc/ssh/sshd_config && \
+    echo "AllowGroups jenkins" >> /etc/ssh/sshd_config
+
+# Comment these lines to disable sudo
+apk --update add sudo && \
+    rm -rf /var/cache/apk/* && \
+    echo "%jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
