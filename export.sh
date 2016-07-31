4b9f57b45c1f68f57428dcd31dbcccbe840b6ba5
shadiakiki1986 <shadiakiki1986@gmail.com>
2016-07-31 07:05:11 +0000

    correct docker image name

--- export.sh
+++ export.sh
@@ -17,11 +17,11 @@ TARGET=$1
 # now export the secrets, as referenced here http://stackoverflow.com/a/31672340/4126114
 # but there is no docker-compose cp yet, as referenced here https://github.com/docker/compose/issues/3593
 function grepCont() {
-  docker ps|grep jenkins2dockershadi_master
+  docker ps|grep jenkins2docker_master
 }
 NCONT=``
 if [ "$(grepCont|wc -l)" != "1" ]; then
-  echo "More than one jenkins master found. Aborting"
+  echo "Not one jenkins master found. Aborting"
   exit 1
 fi
 docker cp `grepCont|awk '{print $1}'`:/var/jenkins_home $TARGET
