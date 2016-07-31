35e4c1989e6d77efcb66b8a18bce1e5400cf363c
Shadi Akiki <shadiakiki1986@gmail.com>
2016-07-31 10:03:34 +0300

    export all jenkins_home

--- export.sh
+++ export.sh
@@ -14,18 +14,6 @@ fi
 
 TARGET=$1
 #------------------------
-# Copy simple files
-function doit() {
-  mkdir -p $TARGET/$(dirname $1)
-  docker-compose exec master cat /var/jenkins_home/$1 > $TARGET/$1
-}
-
-doit nodes/php7/config.xml
-doit jobs/ffa-php-core/config.xml
-doit users/shadi/config.xml
-doit credentials.xml
-
-#------------------------
 # now export the secrets, as referenced here http://stackoverflow.com/a/31672340/4126114
 # but there is no docker-compose cp yet, as referenced here https://github.com/docker/compose/issues/3593
 function grepCont() {
@@ -36,5 +24,5 @@ if [ "$(grepCont|wc -l)" != "1" ]; then
   echo "More than one jenkins master found. Aborting"
   exit 1
 fi
-docker cp `grepCont|awk '{print $1}'`:/var/jenkins_home/secrets $TARGET
-sudo chmod 775 -R $TARGET/secrets
+docker cp `grepCont|awk '{print $1}'`:/var/jenkins_home $TARGET
+sudo chmod 775 -R $TARGET
